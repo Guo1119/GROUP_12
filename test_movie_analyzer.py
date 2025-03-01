db8e2c9e3bcba1621e84eed451dc290a44a1fa26
Leon Klingele <leonklingele@MacBook-Air-von-Leon-5.local>
2025-03-01 14:11:51 +0000

    Applied PEP8 compliance

--- test_movie_analyzer.py
+++ test_movie_analyzer.py
@@ -1,11 +1,13 @@
 import pytest
 from src.movie_analysis import MovieAnalyzer
 
+
 def test_movie_type_invalid_input():
     """Ensure movie_type raises error when input is not an integer."""
     analyzer = MovieAnalyzer()
     with pytest.raises(ValueError):
         analyzer.movie_type("ten")
+
 
 def test_actor_distributions_invalid_input():
     """Ensure actor_distributions raises error when inputs are invalid."""
